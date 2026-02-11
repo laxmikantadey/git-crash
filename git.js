936ea89dc6aadd6db47893e21eb20e4d70faeabb
laxmikantadey <deysuraj898@gmail.com>
2026-02-11 23:19:31 +0530

    new change

---
+++ git.js
@@ -0,0 +1,10 @@
+const age =23;
+const name='suraj'
+let dob='2000-01-01'
+
+function sayName(){
+    console.log(`My name is` + name);
+}
+function sayAge(){
+    console.log(`My age is` + age);
+}
